111da5a6605fd9833d0d9c41c1ec361dd9782c3c
verter1990 <vertervfx@gmail.com>
2025-08-27 16:25:52 +0300

    Remove all files from repository

--- tests/integration/test_user_registration.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import pytest
-from aiogram.types import Message, User as AiogramUser, Chat
-from datetime import datetime
-from sqlalchemy import select
-from core.database.models import User
-from core.handlers.user_handlers import command_start_handler
-from aiogram.filters import CommandObject
